8a94f96233fbe69a42272c6326d62a15f76219bc
Shaan057 <roshansp639@gmail.com>
2023-12-11 12:01:54 +0000

    first commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,72 @@
+import './index.css'
+import {Link, withRouter} from 'react-router-dom'
+import {useContext} from 'react'
+import {HiOutlineSearch} from 'react-icons/hi'
+import Context from '../../Context'
+
+const Header = props => {
+  const {history} = props
+  const context = useContext(Context)
+  const {searchInput, updateSearchInput} = context
+
+  const onSearchButtonClicked = () => {
+    if (searchInput !== '') {
+      //   updateSearchInput(searchInput)
+      history.push(`/search/${searchInput}`)
+    }
+  }
+  const onKeyDownInput = event => {
+    if (event.key === 'Enter' && searchInput !== '') {
+      //   updateSearchInput(event.target.value)
+      history.push(`/search/${searchInput}`)
+    }
+  }
+  const onChangeInput = event => {
+    updateSearchInput(event.target.value)
+  }
+  return (
+    <nav className="nav-bar">
+      <Link className="nav-bar-link" to="/">
+        <h1 className="nav-header">movieDB</h1>
+      </Link>
+      <ul className="nav-bar-navs">
+        <li className="nav-bar-items">
+          <Link className="nav-bar-link" to="/top-rated">
+            <button className="nav-bar-buttons" type="button">
+              Top Rated
+            </button>
+          </Link>
+        </li>
+        <li className="nav-bar-items">
+          <Link className="nav-bar-link" to="/upcoming">
+            <button className="nav-bar-buttons" type="button">
+              Upcoming
+            </button>
+          </Link>
+        </li>
+      </ul>
+      <div className="nav-bar-search-container">
+        <input
+          value={searchInput}
+          placeholder="Search"
+          className="nav-bar-search"
+          type="search"
+          onKeyDown={onKeyDownInput}
+          onChange={onChangeInput}
+        />
+        <Link className="header-search-link" to={`/search/${searchInput}`}>
+          <button
+            className="nav-bar-search-button"
+            type="button"
+            onClick={onSearchButtonClicked}
+          >
+            {/* <HiOutlineSearch className="nav-bar-search-icon" /> */}
+            Search
+          </button>
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
---
+++ src/Components/PopularMoviesListItem/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+import {Link} from 'react-router-dom'
+
+const PopularMoviesListItem = props => {
+  const {data} = props
+  const {posterPath, id, title, voteAverage} = data
+  const imgUrl = `http://image.tmdb.org/t/p/w342${posterPath}`
+  //   const imgUrl = posterPath
+  //     ? `http://image.tmdb.org/t/p/w342${posterPath}`
+  //     : 'http://res.cloudinary.com/dx8csuvrh/image/upload/c_scale,h_513,w_342/v1702224898/samples/ecommerce/HD-wallpaper-pitch-black-dark-phone-plain-solid-thumbnail_brq6us.jpg'
+  return (
+    <li className="popular-movie-item">
+      <div className="popular-movie-img-container">
+        <img className="popular-movie-poster" src={imgUrl} alt={title} />
+      </div>
+      <div className="popular-movie-details">
+        <div className="popular-movie-title-container">
+          <h3 className="popular-movie-title">{title}</h3>
+        </div>
+        <div className="popular-movies-ratings-details">
+          <p className="popular-movie-rating">{voteAverage}</p>
+          <Link to={`/movie/${id}`}>
+            <button className="popular-movie-view-details-button" type="button">
+              View Details
+            </button>
+          </Link>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default PopularMoviesListItem
